75a3dca2620cdd74c5dab5f158017a1c32c3a2ef
SildrePood <mrcooltrash@gmail.com>
2025-02-10 21:43:13 +0200

    Update home.js

--- Pood/home.js
+++ Pood/home.js
@@ -81,7 +81,7 @@ window.onload = function() {
   if (languageToggle) {
     languageToggle.addEventListener('change', function() {
       if (this.checked) {
-        window.location.href = '/est/esthome.html';
+        window.location.href = 'https://sildrepood.github.io/est/esthome.html';
       }
     });
   } else {
